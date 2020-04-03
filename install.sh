c77c7f1fa175dcdc6245b02ddc7f6f5f7a70276f
Magno <magnocarta151@gmail.com>
2020-04-03 19:20:19 -0300

    Correto Agora

--- install.sh
+++ install.sh
@@ -6,5 +6,16 @@ echo "Starting instalation"
 cd ~/Desktop/UriScrapping
 xcodebuild -scheme ArgumentUri build
 
+#chmod u+x ~/Desktop/UriScrapping/oie.txt
 
-`cat ~/Desktop/UriScrapping/oie.txt` >> ~/.zshrc
+echo "alias dat=""\"rm /Users/Shared/Applications/oi.txt\"" >> ~/.zshrc
+echo "alias at=""\"touch /Users/Shared/Applications/oi.txt\"" >> ~/.zshrc
+echo "alias uri=""\"clear &&  /Users/Shared/Applications/ArgumentUri\"" >> ~/.zshrc
+echo "if [ -e /Users/Shared/Applications/oi.txt ]" >> ~/.zshrc
+echo "then" >> ~/.zshrc
+echo        "alias switch=""\"dat\"" >> ~/.zshrc
+echo        "uri" >> ~/.zshrc
+echo "else" >> ~/.zshrc
+echo        "alias switch=""\"at\"" >> ~/.zshrc
+echo "fi" >> ~/.zshrc
+
